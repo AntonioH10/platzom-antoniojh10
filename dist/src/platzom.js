3844643ab97f60b0b8edc4eb6f5bc630efda404f
Antonio Hernandez <antoniohernandez@gmail.com>
2018-06-10 00:32:54 -0400

    babelrc fixed

--- dist/src/platzom.js
+++ dist/src/platzom.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -7,13 +7,25 @@ exports.default = platzom;
 function platzom(str) {
   var translation = str;
 
-  // Si la palabra original es un palíndromo,
-  // ninguna regla anterior cuenta y se devuelve
-  // la misma palabra intercalando mayúsculas y minúsculas
+  // Si la palabra termina con "ar", se le quitan esas dos letras
+  if (str.toLowerCase().endsWith("ar")) {
+    translation = str.slice(0, -2);
+  }
 
-  var reverse = function reverse(str) {
-    return str.split('').reverse().join('');
-  };
+  // Si la palabra inicia con Z, se le añade "pe" al final.
+  if (str.toLowerCase().startsWith("z")) {
+    translation += "pe";
+  }
+
+  // Si la palabra traducida tiene 10 o más letras,
+  // se debe partir en dos por la mitad y unir con un guión medio
+  var length = translation.length;
+  if (length >= 10) {
+    var firstHalf = translation.slice(0, Math.round(length / 2));
+    var secondHalf = translation.slice(Math.round(length / 2));
+    translation = firstHalf + "-" + secondHalf;
+    // translation = [firstHalf, secondHalf].join('-')
+  }
 
   function minMay(str) {
     var length = str.length;
@@ -28,34 +40,16 @@ function platzom(str) {
     return translation;
   }
 
+  function reverse(str) {
+    return str.split('').reverse().join('');
+  }
+
+  // Por último, si la palabra original es un palíndromo,
+  // ninguna regla anterior cuenta y se devuelve la misma palabra pero
+  // intercalando letras mayúsculas y minúsculas
   if (str == reverse(str)) {
     return minMay(str);
   }
 
-  // Si la palabra termina en "ar", se le quitan esos dos caracteres
-  if (str.toLowerCase().endsWith('ar')) {
-    translation = str.slice(0, -2);
-  }
-
-  // Si la palabra inicia con Z, se le añade "pe" al final
-  if (str.toLowerCase().startsWith('z')) {
-    translation += 'pe';
-  }
-
-  // Si la palabra traducida tiene 10 o más letras,
-  // se debe partir a la mitad y unir con un guión del medio
-  var length = translation.length;
-  if (length >= 10) {
-    var firstHalf = translation.slice(0, Math.round(length / 2));
-    var secondHalf = translation.slice(Math.round(length / 2));
-    translation = firstHalf + '-' + secondHalf;
-  }
-
   return translation;
 }
-
-console.log(platzom("Programar")); // Program
-console.log(platzom("Zorro")); // Zorrope
-console.log(platzom("Zarpar")); // Zarppe
-console.log(platzom("abecedario")); // abece-dario
-console.log(platzom("sometemos")); // SoMeTeMoS
